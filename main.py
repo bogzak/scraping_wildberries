7be5d652f08a0be697c0a59edeb701447b69cfd2
bogzak <bogdan.zaakh@gmail.com>
2022-11-26 18:05:07 +0300

    Add files to git

---
+++ main.py
@@ -0,0 +1,15 @@
+import module.collect_links
+import module.get_pages
+import module.get_products
+import module.get_reviews
+
+
+def main():
+    module.get_pages.save_categories()
+    all_links = module.collect_links.get_links()
+    module.get_products.save_products(all_links)
+    module.get_reviews.parse_reviews()
+
+
+if __name__ == '__main__':
+    main()
